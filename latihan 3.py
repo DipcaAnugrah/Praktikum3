abd033cbe42f7411c8e74aebed0ce06b30cdcb15
dipca0895 <dipcaanugrah26@gmail.com>
2022-10-27 07:27:46 +0700

    membuat latihan 3

--- latihan 3.py
+++ latihan 3.py
@@ -1,3 +1,12 @@
 print(" Hello ini script python Dipca Anugrah :)")
-
-print(' {0:>3} {1:>4}'.format(0, 10**0))
+a = "*"
+print(type(a))
+print('{0:>10}'.format(a))
+print('{0:>11}'.format(a*3))
+print('{0:>12}'.format(a*5))
+print('{0:>13}'.format(a*7))
+print('{0:>14}'.format(a*9))
+print('{0:>13}'.format(a*7))
+print('{0:>12}'.format(a*5))
+print('{0:>11}'.format(a*3))
+print('{0:>10}'.format(a))
